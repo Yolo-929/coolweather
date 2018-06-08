0f41f5a312f85ab28c0d6fdd0092e22df7535c12
Yolo <1035706563@qq.com>
2018-06-08 17:24:42 +0800

    加入显示天气信息的功能。

---
+++ src/activity/WeatherActivity.java
@@ -0,0 +1,142 @@
+package activity;
+
+import util.HttpCallbackListener;
+import util.HttpUtil;
+import util.Utility;
+
+import com.coolweather.app.R;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WeatherActivity extends Activity {
+
+	private LinearLayout weatherInfoLayout;
+	/**
+	 * name
+	 */
+	private TextView cityNameText;
+	/**
+	 * time
+	 */
+	private TextView publishText;
+	/**
+	 * info
+	 */
+	private TextView weatherDespText;
+	/**
+	 * temp1
+	 */
+	private TextView temp1Text;
+	/**
+	 * temp2
+	 */
+	private TextView temp2Text;
+	/**
+	 * data
+	 */
+	private TextView currentDateText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.weather_layout);
+		//初始化控件
+		weatherInfoLayout=(LinearLayout)findViewById(R.id.weather_info_layout);
+		cityNameText=(TextView)findViewById(R.id.city_name);
+		publishText=(TextView)findViewById(R.id.publish_text);
+		weatherDespText=(TextView)findViewById(R.id.weather_desp);
+		temp1Text=(TextView)findViewById(R.id.temp1);
+		temp2Text=(TextView)findViewById(R.id.temp2);
+		currentDateText=(TextView)findViewById(R.id.current_date);
+		String countyCode=getIntent().getStringExtra("county_code");
+		if(!TextUtils.isEmpty(countyCode)){
+			publishText.setText("同步中...");
+			weatherInfoLayout.setVisibility(View.INVISIBLE);
+			cityNameText.setVisibility(View.INVISIBLE);
+			queryWeatherCode(countyCode);
+		}else{
+			showWeather();
+		}
+	}
+
+	private void queryWeatherCode(String countyCode) {
+		//查询县级代号的天气代号
+		String address="http://www.weather.com.cn/data/list3/city"+countyCode+".xml";
+		queryFromServer(address,"countyCode");
+	}
+	
+	private void queryWeatherInfo(String weatherCode){
+		String address="http://www.weather.com.cn/data/cityinfo/"+weatherCode+".html";
+		queryFromServer(address,"weatherCode");
+	}
+
+	private void queryFromServer(final String address,final String type) {
+		// TODO Auto-generated method stub
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+			
+			@Override
+			public void onFinish(String response) {
+				// TODO Auto-generated method stub
+				if("countyCode".equals(type)){
+					if(!TextUtils.isEmpty(response)){
+						//从服务器返回的数据中解析天气信号
+						String[] array =response.split("\\|");
+						if(array!=null&&array.length==2){
+							String weatherCode=array[1];
+							queryWeatherInfo(weatherCode);
+						}
+					}
+				}else if("weatherCode".equals(type)){
+					//处理服务器返回的天气信息
+					Utility.handleWeatherResponse(WeatherActivity.this, response);
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							showWeather();
+						}
+					});
+				}
+			}
+			
+			@Override
+			public void onError(Exception e) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						publishText.setText("同步失败");
+					}
+				});
+			}
+		});
+	}
+	
+	/**
+	 * 从文件中读取存储的天气信息，并显示
+	 */
+	private void showWeather(){
+		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+		cityNameText.setText(prefs.getString("city_name", ""));
+		temp1Text.setText(prefs.getString("temp1", ""));
+		temp2Text.setText(prefs.getString("temp2", ""));
+		weatherDespText.setText(prefs.getString("weather_desp", ""));
+		publishText.setText("今天"+prefs.getString("publish_time", ""));
+		currentDateText.setText(prefs.getString("current_date", ""));
+		weatherInfoLayout.setVisibility(View.VISIBLE);
+		cityNameText.setVisibility(View.VISIBLE);
+	}
+}
