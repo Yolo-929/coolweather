25fbef82aa8c03f1129c61a52609895bfc3f8e9b
Yolo <1035706563@qq.com>
2018-06-08 09:33:17 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/model/County.java
@@ -0,0 +1,35 @@
+package model;
+
+public class County {
+	private int id;
+	private String countyName;
+	private String countyCode;
+	private int cityId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountyName() {
+		return countyName;
+	}
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+	public String getCountyCode() {
+		return countyCode;
+	}
+	public void setCountyCode(String countyCode) {
+		this.countyCode = countyCode;
+	}
+	public int getCityid() {
+		return cityId;
+	}
+	public void setCityid(int cityId) {
+		this.cityId = cityId;
+	}
+	
+	
+}
